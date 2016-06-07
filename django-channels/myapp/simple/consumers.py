503d9256de6c4789f9c837e653d087af430a2992
Reina Abolofia <rabolofia@newrelic.com>
2016-06-06 17:12:27 -0700

    remove unneeded stuff

--- django-channels/myapp/simple/consumers.py
+++ django-channels/myapp/simple/consumers.py
@@ -5,12 +5,6 @@ from channels.sessions import channel_session
 @channel_session
 def ws_connect(message):
     print 'connecting...'
-    path = message.content['path']
-    Group(path).add(message.reply_channel)
-    group = Group(path)
-    print 'dir(group): ', dir(group)
-    print 'len(group): ', len(group)
-    print 'there are now %d connections' % 1
 
 @channel_session
 def ws_receive(message):
@@ -19,6 +13,3 @@ def ws_receive(message):
 @channel_session
 def ws_disconnect(message):
     print 'disconnecting...'
-    path = message.content['path']
-    Group(path).discard(message.reply_channel)
-    print 'there are now %d connections' % 1
