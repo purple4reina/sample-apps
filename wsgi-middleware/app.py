9dfe329f3f82e656896b393294141584d8cdd4f8
Reina Abolofia <rabolofia@newrelic.com>
2016-06-27 10:00:35 -0700

    Simplification of wsgi middleware
    Also adds a sample iterator to return instead of a list

--- wsgi-middleware/app.py
+++ wsgi-middleware/app.py
@@ -4,8 +4,33 @@ from middleware import HelloGoodbyer
 def application(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
+
+    # this returns a write() method, but we don't use it
     start_response(status, response_headers)
-    return ['hello world\n']
+
+    # return a type that can be iterated across
+    # this includes lists, strings, tuples, etc
+    #
+    # curl will print the contents of the response as they arrive while the
+    # browser will wait till the connection is closed to render the response
+    return ['Hello world\n']
+    return LongIterable('hello world\n', 10)
+
+
+class LongIterable(object):
+    def __init__(self, return_val, total):
+        self.return_val = return_val
+        self.total = total
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        while self.count < self.total:
+            self.count += 1
+            return self.return_val
+        raise StopIteration
 
 
 wrapped_application = HelloGoodbyer(application)
--- wsgi-middleware/middleware.py
+++ wsgi-middleware/middleware.py
@@ -2,65 +2,40 @@ from transform import hello_to_goodbye
 
 
 class HelloGoodbyeIter:
-
-    """Transform iterated output using hello_to_goodbye, if it's okay to do so
-
-    Note that the "okayness" can change until the application yields
-    its first non-empty string, so 'transform_ok' has to be a mutable
-    truth value.
+    """
+    Transform iterated output using hello_to_goodbye
     """
 
-    def __init__(self, result, transform_ok):
+    def __init__(self, result):
         if hasattr(result, 'close'):
             self.close = result.close
         self._next = iter(result).next
-        self.transform_ok = transform_ok
 
     def __iter__(self):
         return self
 
     def next(self):
-        if self.transform_ok:
-            return hello_to_goodbye(self._next())
-        else:
-            return self._next()
+        # This is where the change is happening
+        return hello_to_goodbye(self._next())
 
 
 class HelloGoodbyer:
 
-    # by default, don't transform output
-    transform = False
-
     def __init__(self, application):
         self.application = application
 
+    # returns an iteratable
     def __call__(self, environ, start_response):
 
-        transform_ok = []
+        # returns a function `write` which takes one argument, a string
+        # (body_data) that is the contents of the page
+        def start_hello_goodbye(status, response_headers):
+            write = start_response(status, response_headers)
 
-        def start_hello_goodbye(status, response_headers, exc_info=None):
+            # transform the text in body_data before sending it to `write`
+            def write_hello_goodbye(body_data):
+                # Why is it that the body is changed both here and in the Iter?
+                return write(hello_to_goodbye(body_data))
+            return write_hello_goodbye
 
-            # Reset ok flag, in case this is a repeat call
-            del transform_ok[:]
-
-            for name, value in response_headers:
-                if name.lower() == 'content-type' and value == 'text/plain':
-                    transform_ok.append(True)
-                    # Strip content-length if present, else it'll be wrong
-                    response_headers = [(name, value)
-                        for name, value in response_headers
-                            if name.lower() != 'content-length'
-                    ]
-                    break
-
-            write = start_response(status, response_headers, exc_info)
-
-            if transform_ok:
-                def write_hello_goodbye(data):
-                    write(hello_to_goodbye(data))
-                return write_hello_goodbye
-            else:
-                return write
-
-        return HelloGoodbyeIter(
-            self.application(environ, start_hello_goodbye), transform_ok)
+        return HelloGoodbyeIter(self.application(environ, start_hello_goodbye))
