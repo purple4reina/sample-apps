c09001f34fc3c67f23633aa737379ea53bfaef8b
Reina Abolofia <rabolofia@newrelic.com>
2016-07-05 10:39:28 -0700

    Update app to use threads

--- external-services/README.md
+++ external-services/README.md
@@ -15,9 +15,11 @@ were not showing up in the External Services tab.
   ```
 
 2. Create small non instrumented package by moving a file to site-packages.
+   Make sure there is no `mytest_package.py` or `mytest_package.pyc` in the
+   current working directory.
 
   ```
-  cp mytest_package.py env/lib/python2.7/site-packages/
+  mv mytest_package.py env/lib/python2.7/site-packages/
   ```
 
 3. Make sure you have newrelic!
@@ -33,3 +35,10 @@ were not showing up in the External Services tab.
   ```
   NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-python run_background_task.py
   ```
+
+## Expected results
+
+The `run_background_task.py` script will attempt to record BackgroundTask's
+that are run inside and outside of threads. When the `with` statement is *inside* the thread,
+the external service (urllib) will appear in the External Services tab. When
+the `with` statement is *outside* the thread, it will not.
--- external-services/run_background_task.py
+++ external-services/run_background_task.py
@@ -1,15 +1,45 @@
 import newrelic.agent
+from threading import Thread
 
 from mytest_package import myfunction
 
 application = newrelic.agent.register_application(timeout=10.0)
 
-def main():
+
+def do_something_with_backgroundtask(num):
     with newrelic.agent.BackgroundTask(application, name=__name__,
-            group='Task'):
-        for _ in xrange(100):
+            group='Present'):
+        for _ in xrange(num):
             myfunction()
 
+def do_something(num):
+    for _ in xrange(num):
+        myfunction()
 
 if __name__ == '__main__':
-    main()
+
+    # Run a task that has the `with` statement inside a new thread.
+    # When the `with newrelic.agent.BackgroundTask` is in the
+    # `do_something_with_backgroundtask` function, the external transaction
+    # gets recorded in APM.
+    thread = Thread(target=do_something_with_backgroundtask, args=(100,))
+    thread.start()
+    thread.join()
+    print 'Finished performing first calculations'
+
+    # Run a similar task where the `with` statement is not in the thread.
+    # When the `with newrelic.agent.BackgroundTask` is in the `if __name__ ==
+    # __main__` section, the external transaction does NOT get recorded in APM.
+    with newrelic.agent.BackgroundTask(application, name=__name__,
+            group='NotPresent'):
+        thread = Thread(target=do_something, args=(100,))
+        thread.start()
+        thread.join()
+    print 'Finished performing second calculations'
+
+    # Lastly, run the task outside of a thread. The external service
+    # transaction gets recorded in APM.
+    with newrelic.agent.BackgroundTask(application, name=__name__,
+            group='NoThread'):
+        do_something(100)
+    print 'Finished performing third calculations'
