7f8737cb1210b713284d65407c9dc46e999b6859
Reina Abolofia <rabolofia@newrelic.com>
2016-08-01 15:44:36 -0700

    Found the problem and reduced it

--- flask-blueprints/app.py
+++ flask-blueprints/app.py
@@ -1,23 +1,11 @@
 import flask
 
-
-print
-blueprint = flask.Blueprint('blueprint', __name__)
-
-@blueprint.route('/')
-def home():
-    return 'Hello'
-
-@blueprint.route('/')
-def yohome():
-    return 'yoHello'
-
 app = flask.Flask(__name__)
 
-app.register_blueprint(blueprint)
-
-print 'app.url_map: ', app.url_map
-
+@app.route('/home')
+@app.route('/info')
+def home():
+    return 'hello world'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
