6b0e2e957f0b37cea338fb6c8c4752994983ac17
Reina Abolofia <rabolofia@newrelic.com>
2016-08-30 12:17:04 -0700

    Change the app name when creating a new one

--- start-app.sh
+++ start-app.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 name=$1
+name_title=`python -c "print '${name}'.replace('-', ' ').replace('_', ' ').title()"`
 
 mkdir $name
 cd $name
 
-cp ../newrelic.ini .
-cp ../requirements.txt .
+cp ../newrelic.ini newrelic.ini
+sed -i '' "s/app_name = .*/app_name = ${name_title}/g" newrelic.ini
 
 virtualenv env
 source env/bin/activate
+cp ../requirements.txt .
 pip install -r requirements.txt
 
 cat > app.py <<EOL
