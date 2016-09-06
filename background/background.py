1ed32fcaea76c5df0c3895407bf21fa431b2b3fa
Reina Abolofia <rabolofia@newrelic.com>
2016-09-06 09:39:47 -0700

    Add WAIT_TIME, and sys.exit

--- background/background.py
+++ background/background.py
@@ -3,17 +3,20 @@ newrelic.agent.initialize('newrelic.ini')
 newrelic.agent.register_application(timeout=10)
 
 import time
+import sys
+
+WAIT_TIME = 0
 
 @newrelic.agent.background_task()
 def main():
-    time.sleep(1)
+    time.sleep(WAIT_TIME)
     a()
     b()
     c()
 
 @newrelic.agent.function_trace()
 def a():
-    time.sleep(1)
+    time.sleep(WAIT_TIME)
     return
 
 @newrelic.agent.function_trace()
@@ -22,8 +25,9 @@ def b():
 
 @newrelic.agent.function_trace()
 def c():
-    time.sleep(1)
+    time.sleep(WAIT_TIME)
     return b()
 
 if __name__ == '__main__':
     main()
+    sys.exit(0)
