5fd38206625ecf68b7e73e50b54e574b30e2d549
Reina Abolofia <rabolofia@newrelic.com>
2016-09-16 09:54:38 -0700

    more

--- databasestores/app.py
+++ databasestores/app.py
@@ -1,9 +1,15 @@
 import flask
+import os
 import psycopg2
 import sqlite3
 import time
 
 app = flask.Flask(__name__)
+
+DBUSER = 'python_agent'
+DOCKER_HOST = os.environ.get('DOCKER_HOST')
+if DOCKER_HOST:
+    DOCKER_HOST = DOCKER_HOST.lstrip('tcp:/').split(':', 1)[0]
 
 @app.route('/')
 def home():
@@ -45,11 +51,18 @@ def psycopg():
 
 def _insert_db(c):
     number = str(time.time()).replace('.', '')[:-11:-1]
-    c.execute("""
-        INSERT INTO people VALUES
-            ('John','Doe',%s),
-            ('Jane','Doe',%s)
-    """, (number, number))
+    try:
+        c.execute("""
+            INSERT INTO people VALUES
+                ('John','Doe',%s),
+                ('Jane','Doe',%s)
+        """, (number, number))
+    except sqlite3.OperationalError:
+        c.execute("""
+            INSERT INTO people VALUES
+                ('John','Doe',?),
+                ('Jane','Doe',?)
+        """, (number, number))
 
 def _create_db(c, conn=None):
     try:
@@ -61,6 +74,17 @@ def _create_db(c, conn=None):
         pass
     except psycopg2.ProgrammingError:
         conn.rollback()
+
+@app.route('/pg-port')
+def pg_port():
+    # connect to postgresql through a different port
+    # start db with:
+    #   `docker run -d --name packnsend-postgresql -p 5433:5432 --network="bridge" postgresql`
+    with psycopg2.connect(
+            database=DBUSER, user=DBUSER, password=DBUSER,
+            host=DOCKER_HOST) as conn:
+        pass
+    return ''
 
 if __name__ == '__main__':
     app.run(debug=True)
