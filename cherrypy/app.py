ec61c2fa8272b4c747305b82013143baa8eb52da
Reina Abolofia <rabolofia@newrelic.com>
2016-12-22 09:54:26 -0800

    run either way

--- cherrypy/app.py
+++ cherrypy/app.py
@@ -10,8 +10,12 @@ class Error(object):
         raise cherrypy.HTTPError(400, 'Badness Happened')
     index.exposed = True
 
-cherrypy.config.update({'engine.autoreload.on': False})
-cherrypy.server.unsubscribe()
-cherrypy.engine.start()
+if __name__ == '__main__':
+    cherrypy.quickstart(HelloWorld())
+else:
+    cherrypy.config.update({'engine.autoreload.on': False})
+    cherrypy.server.unsubscribe()
+    cherrypy.engine.start()
 
-wsgiapp = cherrypy.tree.mount(Error())
+    #wsgiapp = cherrypy.tree.mount(Error())
+    wsgiapp = cherrypy.tree.mount(HelloWorld())
