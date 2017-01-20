6e99d45aa87e9bd5ce62b7b37564bd43a76faaa0
Rey Abolofia <rabolofia@newrelic.com>
2017-01-20 13:32:36 -0800

    make it more simple

--- flask/app.py
+++ flask/app.py
@@ -1,20 +1,10 @@
 import flask
-import time
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello world'
-
-@app.route('/slow')
-def slow_world():
-    time.sleep(5)
-    return 'Hello Slow World!'
-
-@app.route('/error')
-def error():
-    raise Exception
+    return '*'
 
 if __name__ == '__main__':
     app.run(debug=True)
