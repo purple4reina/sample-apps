da902c3f5a4e87e67fa954c602484bb18db4f144
Rey Abolofia <rabolofia@newrelic.com>
2017-03-15 10:10:33 -0700

    flake8

--- background/background.py
+++ background/background.py
@@ -1,11 +1,13 @@
+import time
+import sys
+
 import newrelic.agent
 newrelic.agent.initialize('newrelic.ini')
 newrelic.agent.register_application(timeout=10)
 
-import time
-import sys
 
 WAIT_TIME = 0
+
 
 @newrelic.agent.background_task()
 def main():
@@ -14,19 +16,23 @@ def main():
     b()
     c()
 
+
 @newrelic.agent.function_trace()
 def a():
     time.sleep(WAIT_TIME)
     return
 
+
 @newrelic.agent.function_trace()
 def b():
     return a()
+
 
 @newrelic.agent.function_trace()
 def c():
     time.sleep(WAIT_TIME)
     return b()
+
 
 if __name__ == '__main__':
     main()
