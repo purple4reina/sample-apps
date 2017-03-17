096bdb94a05fbdb3c2e91073ab7a876b1e470406
Rey Abolofia <rabolofia@newrelic.com>
2017-03-17 16:18:41 -0700

    some stuff

--- cherrypy/start_app.sh
+++ cherrypy/start_app.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-ARGS='
+UWSGI='../../external_packages/uwsgi/uwsgi'
+BEHAVIOR=cheat
+
+echo "Using behavior $BEHAVIOR"
+
+ARGS="
     --socket 127.0.0.1:8080
     --protocol http
     --wsgi-file app.py
     --callable wsgiapp
     --enable-threads
     --single-interpreter
-    --wsgi-env-behavior holy
     --honour-stdin
-'
+    --wsgi-env-behavior $BEHAVIOR
+"
 
 if [[ -n $WITH_GDB ]]
 then
-    CMD="gdb --args env/bin/uwsgi $ARGS"
+    CMD="gdb --args $UWSGI $ARGS"
 else
-    CMD="env/bin/uwsgi $ARGS"
+    CMD="$UWSGI $ARGS"
 fi
 
 echo $CMD
