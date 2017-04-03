e5b07be11b036a827691e5d8bf31ef967bda1714
Rey Abolofia <rabolofia@newrelic.com>
2017-04-03 14:15:04 -0700

    fuxip

--- tornado4/app.py
+++ tornado4/app.py
@@ -1,31 +1,26 @@
 import time
-
 import tornado.gen
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.httpclient
 
-from newrelic.agent import FunctionTrace, current_transaction, function_trace
-
 
 class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         self.write('*')
-        time.sleep(1)
+        time.sleep(0.1)
         self.finish()
 
 
 class FetchHandler(tornado.web.RequestHandler):
-    #@function_trace(terminal=True)
     @tornado.gen.coroutine
     def get(self):
-        transaction = current_transaction()
-        with FunctionTrace(transaction, 'TRACE'):
-            client = tornado.httpclient.AsyncHTTPClient()
-            resp = yield client.fetch('http://localhost:8888')
-            print 'resp.code: ', resp.code
+        client = tornado.httpclient.AsyncHTTPClient()
+        resp = yield client.fetch('http://localhost:8888')
+        self.write('*')
+        print 'resp.code: ', resp.code
 
 
 def make_app():
