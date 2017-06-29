482e059bb2530d5735d7815dd2a42f65e1c4fe88
Rey Abolofia <rabolofia@newrelic.com>
2017-06-29 14:39:40 -0700

    asdfkdlfjsld

---
+++ rabbitmq/fib_client.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
+newrelic.agent.register_application(timeout=10.0)
+
+import pika
+import uuid
+
+class FibonacciRpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return int(self.response)
+
+fibonacci_rpc = FibonacciRpcClient()
+
+@newrelic.agent.background_task()
+def doit(n):
+    print(" [x] Requesting fib(%s)" % n)
+    response = fibonacci_rpc.call(n)
+    print(" [.] Got %r" % response)
+
+doit(32)
+doit(30)
