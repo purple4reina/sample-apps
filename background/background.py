3fb53c13fff48208a4d6c815b31ca0d7a09883c6
Rey Abolofia <rabolofia@newrelic.com>
2017-08-01 13:48:13 -0700

    an example that uses a transaction

--- background/background.py
+++ background/background.py
@@ -4,9 +4,19 @@ newrelic.agent.initialize('newrelic.ini')
 newrelic.agent.register_application(timeout=10.0)
 
 
+def func():
+    pass
+
+
 @newrelic.agent.background_task()
 def main():
-    pass
+    transaction = newrelic.agent.current_transaction()
+
+    if not transaction:
+        return
+
+    with newrelic.agent.FunctionTrace(transaction, '__main__:func'):
+        return func()
 
 
 if __name__ == '__main__':
