72b8edb490d3f0604199ea73110abd8631ab13a1
Rey Abolofia <rabolofia@newrelic.com>
2017-08-02 16:20:56 -0700

    example of wrap_wsgi_application

--- wsgi/application.py
+++ wsgi/application.py
@@ -1,5 +1,12 @@
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
+
+
 def application(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return [b'*']
+
+
+newrelic.agent.wrap_wsgi_application(__name__, 'application')
