9235a5da3dde909527a9c0e4def874f59f856e5f
Rey Abolofia <rabolofia@newrelic.com>
2017-08-02 14:01:58 -0700

    tasty yummy pie

--- django-tastypie/api.py
+++ django-tastypie/api.py
@@ -1,4 +1,5 @@
 from tastypie.resources import ModelResource
+from tastypie.exceptions import NotFound
 from django.contrib.auth.models import User
 
 
@@ -6,3 +7,9 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+
+    def get_detail(self, request, **kwargs):
+        response = super(UserResource, self).get_detail(request, **kwargs)
+        if response.status_code == 404:
+            raise NotFound('I am raising an error!')
+        return response
