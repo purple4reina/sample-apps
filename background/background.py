5cdcd7dad15249b2987aef4be34eb0f0918c6ae4
Rey Abolofia <rabolofia@newrelic.com>
2017-08-02 14:07:17 -0700

    make it simple again

--- background/background.py
+++ background/background.py
@@ -4,19 +4,10 @@ newrelic.agent.initialize('newrelic.ini')
 newrelic.agent.register_application(timeout=10.0)
 
 
-def func():
-    pass
-
 
 @newrelic.agent.background_task()
 def main():
-    transaction = newrelic.agent.current_transaction()
-
-    if not transaction:
-        return
-
-    with newrelic.agent.FunctionTrace(transaction, '__main__:func'):
-        return func()
+    pass
 
 
 if __name__ == '__main__':
