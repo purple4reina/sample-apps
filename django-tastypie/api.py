576a8441c0bf08f1994a6b6a289595a9e6846ac0
Rey Abolofia <rabolofia@newrelic.com>
2017-08-03 10:37:28 -0700

    Actually do it the right way

--- django-tastypie/api.py
+++ django-tastypie/api.py
@@ -1,15 +1,11 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import Resource
 from tastypie.exceptions import NotFound
 from django.contrib.auth.models import User
 
 
-class UserResource(ModelResource):
+class UserResource(Resource):
     class Meta:
-        queryset = User.objects.all()
         resource_name = 'user'
 
-    def get_detail(self, request, **kwargs):
-        response = super(UserResource, self).get_detail(request, **kwargs)
-        if response.status_code == 404:
-            raise NotFound('I am raising an error!')
-        return response
+    def obj_get(self, *args, **kwargs):
+        raise NotFound()
