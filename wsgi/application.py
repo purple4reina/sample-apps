9592b0f17eebe1a1fae6d9dd0568e1f4e20245ad
Rey Abolofia <rabolofia@newrelic.com>
2017-08-03 15:03:38 -0700

    make it like some classes

--- wsgi/application.py
+++ wsgi/application.py
@@ -1,5 +1,13 @@
-def application(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    return [b'*']
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
+
+
+class Application(object):
+
+    def application(self, environ, start_response):
+        status = '200 OK'
+        response_headers = [('Content-type', 'text/plain')]
+        start_response(status, response_headers)
+        return [b'*']
+
+newrelic.agent.wrap_wsgi_application(__name__, 'Application.application')
