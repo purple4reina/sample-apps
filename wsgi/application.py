abeb8264f1802447abfe476048d1e43578f677b3
Rey Abolofia <rabolofia@newrelic.com>
2017-08-08 16:21:12 -0700

    make it simple again thanks

--- wsgi/application.py
+++ wsgi/application.py
@@ -1,11 +1,13 @@
 import newrelic.agent
 newrelic.agent.initialize('newrelic.ini')
+newrelic.agent.register_application(timeout=100000)
 
-class Application(object):
-    def application(self, environ, start_response):
-        status = '200 OK'
-        response_headers = [('Content-type', 'text/plain')]
-        start_response(status, response_headers)
-        return [b'Hello World']
 
-newrelic.agent.wrap_wsgi_application(__name__, 'Application.application')
+def application(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return [b'Hello World']
+
+
+application = newrelic.agent.WSGIApplicationWrapper(application, name='QWERTY')
