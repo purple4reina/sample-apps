2c0ecc2c6624ef4ddaad78dc01aef0dde898bdfc
Rey Abolofia <rabolofia@newrelic.com>
2017-08-10 14:55:25 -0700

    send method thing

--- grpc/route_guide_client.py
+++ grpc/route_guide_client.py
@@ -4,8 +4,13 @@ import route_guide_pb2
 import route_guide_pb2_grpc
 
 
-channel = grpc.insecure_channel('localhost:50051')
-stub = route_guide_pb2_grpc.RouteGuideStub(channel)
-feature = stub.GetFeature(route_guide_pb2.Point(latitude=409146138,
-    longitude=-746188906))
-print 'feature.name: ', feature.name
+def send():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = route_guide_pb2_grpc.RouteGuideStub(channel)
+    feature = stub.GetFeature(route_guide_pb2.Point(latitude=409146138,
+        longitude=-746188906))
+    print 'feature.name: ', feature.name
+
+
+if __name__ == '__main__':
+    send()
