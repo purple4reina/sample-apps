074bd150e7e3b0a59eb3e90c595dcc6708f0d891
Rey Abolofia <rabolofia@newrelic.com>
2017-09-13 14:07:20 -0700

    More fun for printing

--- utils/decorators.py
+++ utils/decorators.py
@@ -6,6 +6,24 @@ from newrelic.agent import transient_function_wrapper
 def print_nice_transaction_trace():
 
     def _print_metrics(node, level=0):
+        def print_headings():
+            print('name', end='\t')
+            print('start', end='\t')
+            print('end', end='\t')
+            print('start - end', end='\t')
+            print('exclusive', end='\t')
+            print()
+
+        def print_start():
+            start = round(node.start_time, 3)
+            start = str(start).split('.')[0][-3:] + '.' + str(start).split('.')[1]
+            print(start, end='\t')
+
+        def print_end():
+            end = round(node.end_time, 3)
+            end = str(end).split('.')[0][-3:] + '.' + str(end).split('.')[1]
+            print(end, end='\t')
+
         def print_node_time():
             mytime = node.end_time - node.start_time
             print(round(mytime, 3), end='\t')
@@ -15,11 +33,16 @@ def print_nice_transaction_trace():
 
         if level == 0:
             name = node.base_name
+            print_headings()
         elif hasattr(node, 'name'):
             name = node.name
+        elif hasattr(node, 'url'):
+            name = node.url
         else:
             name = node.operation
         print('    ' * level + name, end='\t')
+        print_start()
+        print_end()
         print_node_time()
         print_exclusive_time()
         print()
