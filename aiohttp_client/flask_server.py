ea7fdd1fa2dde989cfb30198016f5130af7d9603
Rey Abolofia <rabolofia@newrelic.com>
2017-09-15 10:29:34 -0700

    simplify the flask server

--- aiohttp_client/flask_server.py
+++ aiohttp_client/flask_server.py
@@ -1,40 +1,13 @@
-import time
 import flask
 
 app = flask.Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     req = flask.request
     print('req.headers: ', req.headers)
     return '*'
-
-
-@app.route('/sleep')
-def sleep():
-    time.sleep(1)
-    return '*'
-
-
-@app.route('/redirect')
-def redirect():
-    return flask.redirect(flask.url_for('redirected'))
-
-
-@app.route('/redirected')
-def redirected():
-    return flask.redirect(flask.url_for('index'))
-
-
-@app.route('/raises')
-def raises():
-    1 / 0
-
-
-@app.route('/chunked')
-def chunked():
-    return '*' * 1000000000
 
 
 if __name__ == '__main__':
