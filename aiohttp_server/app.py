f73fac83a24b0fd45b7b38a815f17d9d350b0e7c
Rey Abolofia <rabolofia@newrelic.com>
2018-02-28 11:31:30 -0800

    make app smaller

--- aiohttp_server/app.py
+++ aiohttp_server/app.py
@@ -1,15 +1,7 @@
 from aiohttp import web
-import asyncio
-
-
-async def doit(num):
-    await asyncio.sleep(0)
-    return num
 
 
 async def handle(request):
-    coros = [doit(i) for i in range(10)]
-    await asyncio.gather(*coros)
     return web.Response(text='*')
 
 
