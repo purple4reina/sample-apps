138444958dd9cb7506ee248092287ee6b9af1788
Rey Abolofia <rabolofia@newrelic.com>
2018-03-05 11:12:21 -0800

    simple aiohttp client

--- aiohttp_client/client.py
+++ aiohttp_client/client.py
@@ -5,26 +5,25 @@ newrelic.agent.register_application(timeout=10.0)
 import aiohttp
 import asyncio
 
-URLS = ['http://0.0.0.0:5000']
+URLS = ['https://example.com', 'https://example.org']
 
 
-async def fetch(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, timeout=0.1) as response:
-            print('url: ', url)
-            print('response.headers: ', response.headers)
-            return await response.text()
+async def fetch(url, client):
+    async with client.get(url) as resp:
+        return await resp.text()
 
 
-async def fetch_multiple():
-    coros = [fetch(url) for url in URLS]
-    return await asyncio.gather(*coros)
+async def multi_fetch():
+    async with aiohttp.ClientSession(raise_for_status=True) as client:
+        text_coros = [fetch(u, client) for u in URLS]
+        return await asyncio.gather(*text_coros)
 
 
 @newrelic.agent.background_task()
 def main():
     loop = asyncio.get_event_loop()
-    text_list = loop.run_until_complete(fetch_multiple())
+    text_list = loop.run_until_complete(multi_fetch())
+    print('text_list: ', text_list)
 
 
 if __name__ == '__main__':
