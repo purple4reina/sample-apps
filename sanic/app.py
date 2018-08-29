de5c91a91b217e88466293f816cd0008949194e7
Rey Abolofia <rabolofia@newrelic.com>
2018-08-29 10:44:58 -0700

    Remove unused endpoints now

--- sanic/app.py
+++ sanic/app.py
@@ -9,19 +9,5 @@ async def index(request):
     return json({'hello': 'world'})
 
 
-@app.middleware('request')
-async def request_middleware(request):
-    return json({'oops': 'wrong'})
-
-
-@app.websocket('/ws')
-async def feed(request, ws):
-    data = 'hello!'
-    print('Sending: ' + data)
-    await ws.send(data)
-    data = await ws.recv()
-    print('Received: ' + data)
-
-
 if __name__ == '__main__':
     app.run()
