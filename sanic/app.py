6cdbcfa7e378f013e8da18602bead240b49bddf2
Rey Abolofia <rabolofia@newrelic.com>
2018-08-31 13:42:14 -0700

    Sanic CAT.

--- sanic/app.py
+++ sanic/app.py
@@ -1,13 +1,25 @@
+import aiohttp
+
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, stream
 
 app = Sanic()
 
 
 @app.route('/')
 async def index(request):
-    return json({'hello': 'world'})
+    async def streaming_fn(response):
+        response.write('foo')
+        response.write('bar')
+    return stream(streaming_fn)
+
+
+@app.route('/cat')
+async def cat(request):
+    async with aiohttp.ClientSession(raise_for_status=True) as client:
+        async with client.get('http://localhost:8000/') as resp:
+            return json(dict((key, value) for key, value in resp.headers.items()))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
