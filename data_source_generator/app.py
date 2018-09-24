6a634b4f497be3e9d75056f1f1fc24b12fe06be8
Rey Abolofia <rabolofia@newrelic.com>
2018-09-24 15:53:25 -0700

    Add a non-decorator version data source.

--- data_source_generator/app.py
+++ data_source_generator/app.py
@@ -7,11 +7,26 @@ def custom_metrics():
     yield ('Custom/Rey/Time', time.time())
 
 
+def custom_source(settings):
+    def custom_metrics():
+        yield ('Custom/Reina/Time', time.time())
+
+    def custom_metrics_factory(environ):
+        return custom_metrics
+
+    properties = {}
+    properties['factory'] = custom_metrics_factory
+    properties['name'] = 'Reina Time'
+
+    return properties
+
+
 if __name__ == '__main__':
     newrelic.agent.initialize()
     newrelic.agent.register_application()
     newrelic.agent.register_data_source(custom_metrics)
+    newrelic.agent.register_data_source(custom_source)
 
     # run forever
     while True:
-        pass
+        time.sleep(1)
