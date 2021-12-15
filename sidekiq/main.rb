e6e7bb9d3d0aed9bcdbff177b36b7e6f17dac455
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-15 10:25:40 -0800

    Sample sidekiq app.

---
+++ sidekiq/main.rb
@@ -0,0 +1,35 @@
+require 'sidekiq'
+require 'sidekiq-status'
+
+Sidekiq.configure_client do |config|
+  Sidekiq::Status.configure_client_middleware config, expiration: 300
+end
+Sidekiq.configure_server do |config|
+  Sidekiq::Status.configure_server_middleware config, expiration: 300
+  Sidekiq::Status.configure_client_middleware config, expiration: 300
+end
+
+class ExampleJob
+  include Sidekiq::Worker
+  include Sidekiq::Status::Worker
+
+  sidekiq_options retry: 0
+
+  def perform(howlong=5)
+    puts "--------------------WORKER STARTING---------------------"
+    sleep howlong
+    store(slept: howlong)
+    asf
+    puts "--------------------WORKER DONE---------------------"
+  end
+end
+
+if __FILE__ == $0
+  job_id = ExampleJob.perform_async(3)
+
+  30.times do
+    status = Sidekiq::Status::get_all(job_id)
+    puts "job_id: #{job_id}, status: #{status.inspect}"
+    sleep 1
+  end
+end
