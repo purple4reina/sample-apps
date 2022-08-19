f6452e1f27db29ae4551cf1f46ad4011e361904c
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-08-19 13:15:03 -0700

    .NET lambda function example.

---
+++ dotnet/lambda/README.md
@@ -0,0 +1,25 @@
+# .NET Lambda function
+
+1. Add the templates
+
+    ```bash
+    $ dotnet new -i Amazon.Lambda.Templates
+    ```
+
+2. Install tools
+
+    ```bash
+    $ dotnet tool install -g Amazon.Lambda.Tools
+    ```
+
+    Or update them
+
+    ```bash
+    $ dotnet tool update -g Amazon.Lambda.Tools
+    ```
+
+3. Build the zip
+
+    ```bash
+    $ dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package ./handler.zip
+    ```
---
+++ dotnet/lambda/ReyFunction/src/ReyFunction/Function.cs
@@ -0,0 +1,22 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace ReyFunction;
+
+public class Function
+{
+
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(string input, ILambdaContext context)
+    {
+        LambdaLogger.Log("Hello World!");
+        return input.ToUpper();
+    }
+}
