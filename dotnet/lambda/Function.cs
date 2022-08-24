8a887772ea56f51d61e54eb2747e49cd69e62c76
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-08-24 09:00:41 -0700

    Give generic name to handler.

--- dotnet/lambda/Function.cs
+++ dotnet/lambda/Function.cs
@@ -4,7 +4,7 @@ using Datadog.Trace;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace ReyFunction;
+namespace MyFunction;
 
 public class Function
 {
