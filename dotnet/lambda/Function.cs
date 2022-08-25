f2ea3df3b7f96b7c7cdb35b61f776d4d8f6e8f73
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-08-25 13:09:26 -0700

    Change signature.

--- dotnet/lambda/Function.cs
+++ dotnet/lambda/Function.cs
@@ -1,6 +1,7 @@
-using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Datadog.Trace;
+using System.Net;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -11,7 +12,8 @@ public class Function
 {
     static readonly HttpClient client = new HttpClient();
 
-    public async Task<string> FunctionHandler(Dictionary < string, string > input, ILambdaContext context)
+    //public async Task<string> FunctionHandler(Dictionary < string, string > input, ILambdaContext context)
+    public async Task<string> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
     {
         LambdaLogger.Log("[Debug] FunctionHandler executing");
         using (var scope = Tracer.Instance.StartActive("my-span"))
