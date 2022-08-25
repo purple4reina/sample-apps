c7c2fb16694ebd12ea2b575262c444d20da8c9fc
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-08-25 13:29:26 -0700

    Add timestamp to deploy script.

--- dotnet/lambda/build_deploy.sh
+++ dotnet/lambda/build_deploy.sh
@@ -5,4 +5,5 @@ time (
   dotnet lambda package --configuration Release --output-package ./handler.zip
   echo "🚀 deploying package"
   aws-vault exec sandbox-account-admin -- sls deploy
+  echo "🎉 deploy complete at $(date)"
 )
