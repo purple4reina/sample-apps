de6bb8716fd1232c3e241bbb5a09e651d7561c83
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-09-02 09:55:04 -0700

    Script to build the dotnet tracer layer as a zip file.

---
+++ dotnet/utils/build_tracer_layer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+
+CURRENT_DIR=`pwd`
+DOTNET_TRACER_DIR=$HOME/dd/dd-trace-dotnet
+
+(
+    # if is not symlink
+    if [ ! -h /ddlogs ]; then
+        echo "Directory /ddlogs does not exist!  On mac, create one using:"
+        echo "https://www.igorkromin.net/index.php/2020/02/19/how-to-create-symlinks-at-the-macos-1015-catalina-root-file-system/"
+        exit 1
+    fi
+
+    cd $DOTNET_TRACER_DIR/tracer
+    # force linux/amd64 docker containers to avoid problems with arm64
+    DOCKER_DEFAULT_PLATFORM=linux/amd64 ./build_in_docker.sh BuildTracerHome
+
+    cd $CURRENT_DIR
+    rm -rf datadog dotnet-tracer.zip
+    cp -r $DOTNET_TRACER_DIR/shared/bin/monitoring-home $CURRENT_DIR/datadog
+    zip -r dotnet-tracer.zip datadog
+)
