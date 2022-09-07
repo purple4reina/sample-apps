372e4e7f571bef16e22402844011e21d2213f051
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-09-07 15:09:14 -0700

    Remove dynamic tag.

--- dotnet/lambda/dynamic_tags.js
+++
@@ -1,8 +0,0 @@
-module.exports.datetime = function() {
-  var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  var dateTime = date+'T'+time;
-  console.log("⏰ deploying serverless function at:", dateTime);
-  return 'deploy_time:'+dateTime;
-}();
