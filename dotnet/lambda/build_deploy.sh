a0473df626c388d4d5a9a73e5d0e41f8acc0976e
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-09-14 09:32:40 -0700

    Hide handler.zip file.

--- dotnet/lambda/build_deploy.sh
+++ dotnet/lambda/build_deploy.sh
@@ -3,7 +3,7 @@
 time (
   echo "📦 packaging release"
   echo
-  dotnet lambda package --configuration Release --output-package ./handler.zip
+  dotnet lambda package --configuration Release --output-package .package/handler.zip
   echo
   echo "🚀 deploying package"
   aws-vault exec sandbox-account-admin -- sls deploy
