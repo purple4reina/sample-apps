6fe8de2ea0da9379dcf0c6f3ade0221fd5015590
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-09-21 13:26:19 -0700

    Change where debug logs are turned on.

--- python/lambda/handler.py
+++ python/lambda/handler.py
@@ -1,3 +1,7 @@
 def handler(event, context):
     print(f'executing lambda handler with event {event} and context {context}')
-    return {'Hello': 'World!'}
+    ret = {'Hello': 'World!'}
+    try:
+        return ret
+    finally:
+        print(f'returned {ret}')
