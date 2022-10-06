b3beffdce7bcc5447e00bb8b0a6198438863a6d9
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-10-06 08:33:06 -0700

    Remove extra copy of extension.

--- python/telemetry-api-demo/extension/telemetry_processor.py
+++
@@ -1,7 +0,0 @@
-def process_telmetery(queue):
-    while not queue.empty():
-        print ("[telmetry_processor] Process telemetry data")
-        batch = queue.get_nowait()
-
-        # Modify the below line to dispatch/send the telemetry data to the desired choice of observability tool.
-        print(f"BATCH RECEIVED: {batch}", flush=True)
