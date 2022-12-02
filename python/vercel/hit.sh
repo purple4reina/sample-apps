26c294cd4e845064206e30c8dc30fd19a17bb7a8
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-12-02 13:24:51 -0800

    Script to smash the endpoints.

---
+++ python/vercel/hit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+urls="https://uh6ghrre7kdrctvbbz75eb24nm0ppbzf.lambda-url.sa-east-1.on.aws https://vkf7w2xarfu56yf5rcthdauzt40acobw.lambda-url.sa-east-1.on.aws https://546bvtv2dawngj5dyeabxbk6le0hizfm.lambda-url.sa-east-1.on.aws https://xqbr2re6uml2ahubczz4hra73y0berzu.lambda-url.sa-east-1.on.aws"
+
+while true
+do
+    for api_key in $DD_API_KEY $DD_PERSONAL_API_KEY
+    do
+        for url in $urls
+        do
+            echo
+            echo "api_key: $api_key"
+            echo "url:     $url"
+            curl $url/$api_key/datadoghq.com &
+        done
+    done
+
+    sleep 1
+done
