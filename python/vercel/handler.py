eaa4e407d8d28f2f9932858755c4bbf76e679879
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-12-02 12:04:38 -0800

    Ready for end to end testing.

--- python/vercel/handler.py
+++ python/vercel/handler.py
@@ -1,30 +1,80 @@
 import os
-import secrets
 
 if os.environ.get('REY_MANUALLY_INSTRUMENT') == 'true':
     import ddtrace
     ddtrace.patch_all()
 
 import json
+import random
+import secrets
 import urllib.request
 
 url = 'http://127.0.0.1:4318/v1/traces'
-with open('trace.json') as f:
-    trace = json.load(f)
-span = trace['resourceSpans'][0]['scopeSpans'][0]['spans'][0]
+
+_words = {}
+def randomized_word(api_key):
+    if api_key not in _words:
+        with open('/usr/share/dict/words') as f:
+            ws = f.read().split('\n')
+        _words[api_key] = random.choice(ws)
+    return _words[api_key]
+
+def trace_dict(api_key):
+    name = randomized_word(api_key)
+    return {
+        "resourceSpans": [
+            {
+                "resource": {
+                    "attributes": [
+                        {
+                            "key": "service.name",
+                            "value": {
+                                "stringValue": "service-" + name
+                            }
+                        }
+                    ]
+                },
+                "scopeSpans": [
+                    {
+                        "instrumentationLibrary": {
+                            "name": "manual-test"
+                        },
+                        "spans": [
+                            {
+                                "traceId": secrets.token_hex(16),
+                                "spanId": secrets.token_hex(8),
+                                "name": "span-" + name,
+                                "kind": 2,
+                                "droppedAttributesCount": 0,
+                                "events": [],
+                                "attributes": [
+                                    {
+                                        "key": "attr1",
+                                        "string_value": "attr1-" + name,
+                                    },
+                                    {
+                                        "key": "attr2",
+                                        "string_value": "attr2-" + name,
+                                    }
+                                ],
+                                "droppedEventsCount": 0,
+                                "status": {
+                                    "code": 1
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
 
 def handler(event={}, context={}):
     api_key, site = event.get('rawPath').strip('/').split('/')
     print(f'handler submitting trace to "{url}" with api key "{api_key}" '
           f'and site "{site}"')
 
-    trace_id = secrets.token_hex(16)
-    span_id = secrets.token_hex(8)
-    print(f'using trace_id "{trace_id}" and span_id "{span_id}"')
-    span['traceId'] = trace_id
-    span['spanId'] = span_id
-
-    trace_json = json.dumps(trace)
+    trace_json = json.dumps(trace_dict(api_key))
     print(f'posting request body:\n{trace_json}')
     req = urllib.request.Request(
             url,
@@ -41,7 +91,7 @@ def handler(event={}, context={}):
         with urllib.request.urlopen(req) as f:
             return {
                     'statusCode': f.status,
-                    'body': f.read().decode() or 'ok',
+                    'body': trace_json,
             }
 
     except Exception as e:
@@ -54,5 +104,6 @@ def handler(event={}, context={}):
 
 if __name__ == '__main__':
     import sys
+    url = 'https://example.com'
     event = {'rawPath': sys.argv[1] + '/' + sys.argv[2]}
     print(handler(event))
