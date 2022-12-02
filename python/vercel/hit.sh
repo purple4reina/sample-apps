4a1d2b07d8065c0167c68540fa7ce9b92dfe5298
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-12-02 13:26:49 -0800

    Clean up arrays.

--- python/vercel/hit.sh
+++ python/vercel/hit.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-urls="https://uh6ghrre7kdrctvbbz75eb24nm0ppbzf.lambda-url.sa-east-1.on.aws https://vkf7w2xarfu56yf5rcthdauzt40acobw.lambda-url.sa-east-1.on.aws https://546bvtv2dawngj5dyeabxbk6le0hizfm.lambda-url.sa-east-1.on.aws https://xqbr2re6uml2ahubczz4hra73y0berzu.lambda-url.sa-east-1.on.aws"
+keys="
+    $DD_API_KEY
+    $DD_PERSONAL_API_KEY
+"
+urls="
+    https://uh6ghrre7kdrctvbbz75eb24nm0ppbzf.lambda-url.sa-east-1.on.aws
+    https://vkf7w2xarfu56yf5rcthdauzt40acobw.lambda-url.sa-east-1.on.aws
+    https://546bvtv2dawngj5dyeabxbk6le0hizfm.lambda-url.sa-east-1.on.aws
+    https://xqbr2re6uml2ahubczz4hra73y0berzu.lambda-url.sa-east-1.on.aws
+"
 
 while true
 do
-    for api_key in $DD_API_KEY $DD_PERSONAL_API_KEY
+    for api_key in $keys
     do
         for url in $urls
         do
