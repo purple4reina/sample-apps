d25571ab54ae12b0d16778dbaf1199389bb43488
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-12-14 09:50:39 -0800

    Simplify the simple.

--- python/lambda/handler.py
+++ python/lambda/handler.py
@@ -1,25 +1,6 @@
-import functools
-import time
-
-def log_request_response(fn):
-    @functools.wraps(fn)
-    def log(event, context):
-        start = time.time()
-        print(f'executing lambda handler {fn.__name__} with event {event} and context {context}')
-        try:
-            ret = fn(event, context)
-            print(f'handler returned {ret}')
-            return ret
-        except Exception as e:
-            print(f'handler raised exception: [{e.__class__.__name__}] {e}')
-            raise
-        finally:
-            end = time.time()
-            print(f'total time: {end-start}')
-    return log
-
-@log_request_response
 def handler(event, context):
+    print('handler received event:', event)
+    print('handler received context:', context)
     return {
             'statusCode': 200,
             'body': '{"Hello": "World"}',
