b8f689f0d1742c1c5fd06e5027b9192ceb76d4e5
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-01-19 13:22:21 -0800

    Send traces to locally running datadog agent.

--- python/otlp/handler.py
+++ python/otlp/handler.py
@@ -1,25 +1,27 @@
 from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
 resource = Resource(attributes={
     SERVICE_NAME: 'rey-python-otlp',
 })
 
 provider = TracerProvider(resource=resource)
-processor = BatchSpanProcessor(ConsoleSpanExporter())
+#processor = BatchSpanProcessor(ConsoleSpanExporter())
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint='http://localhost:4318/v1/traces'))
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 
 tracer = trace.get_tracer(__name__)
 
+@tracer.start_as_current_span('handler')
 def handler(event=None, context=None):
-    with tracer.start_as_current_span('handler') as span:
-        return {
-                'statusCode': 200,
-                'body': '{"Hello": "World"}',
-        }
+    return {
+            'statusCode': 200,
+            'body': '{"Hello": "World"}',
+    }
 
 if __name__ == '__main__':
     handler()
