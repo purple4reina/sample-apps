85b4abfb6a14e02f80b1c278ecf8a3e7c71ffb39
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-03-08 13:03:10 -0800

    Log cold starts.

--- node/lambda/handler.js
+++ node/lambda/handler.js
@@ -1,3 +1,9 @@
-exports.handler = function(event, context) {
-  return { hello: 'world' };
+var coldStart = true;
+
+exports.handler = async function(event, context) {
+  try {
+    return { cold_start: coldStart }
+  } finally {
+    coldStart = false;
+  }
 }
