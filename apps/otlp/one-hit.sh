6669b958c8bee8ce754d99b68cdadbe25459a90e
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-05-19 12:49:41 -0700

    remove java

--- apps/otlp/one-hit.sh
+++ apps/otlp/one-hit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 curl https://6phy3nfh2cotpin3mrlnke5pj40xerdo.lambda-url.sa-east-1.on.aws & # golang
-curl https://6wihadmde7gtw75f2ma2fz2cyu0izjgp.lambda-url.sa-east-1.on.aws & # java
 curl https://jv7orak6kr7cdrb7jedwedz7240rylcf.lambda-url.sa-east-1.on.aws & # node
 curl https://alxr7cqpbc5es3gumio7hph2sy0uqkxh.lambda-url.sa-east-1.on.aws & # python
 wait ; echo
