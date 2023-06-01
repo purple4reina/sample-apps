d18e931e47b948d0c6242c35e7b148a34fc80c4f
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-06-01 10:05:37 -0700

    Remove java building.

--- apps/otlp/deploy.sh
+++ apps/otlp/deploy.sh
@@ -6,30 +6,21 @@ rm -rf "$ARTIFACTS_DIR"
 mkdir -p "$ARTIFACTS_DIR"
 
 # golang
-echo "Building golang 🏌"
+echo "🏌 Building golang"
 GOARCH=amd64 GOOS=linux go build -o "$ARTIFACTS_DIR/handler" golang/handler.go
 zip -j "$ARTIFACTS_DIR/golang.zip" "$ARTIFACTS_DIR/handler"
 
-# java
-echo "Building java ☕️"
-cd "$ROOT_DIR/java"
-./gradlew clean build
-filename=rey-app-otlp-dev-java-dev-all.jar
-cp "build/libs/$filename" "$ARTIFACTS_DIR/java.jar"
-rm -rf build
-cd "$ROOT_DIR"
-
 # node
-echo "Building node 💠"
+echo "💠 Building node"
 cd "$ROOT_DIR/node"
 npm install
 zip -qr "$ARTIFACTS_DIR/node.zip" handler.js instrument.js node_modules
 cd "$ROOT_DIR"
 
 # python
-echo "Building python 🐍"
+echo "🐍 Building python"
 zip -j "$ARTIFACTS_DIR/python.zip" python/handler.py
 
 # deploy
-echo "Deploying 🚀"
+echo "🚀 Deploying"
 aws-vault exec serverless-sandbox-account-admin -- sls deploy
