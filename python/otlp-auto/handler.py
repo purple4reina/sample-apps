74f9e89c45acb073c3877af4e24e1aa08214e5ff
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-06-09 10:15:47 -0700

    Add custom span.

--- python/otlp-auto/handler.py
+++ python/otlp-auto/handler.py
@@ -1,4 +1,12 @@
+import time
+
+from opentelemetry import trace
+
+tracer = trace.get_tracer(__name__)
+
 def handler(event, context):
+    with tracer.start_as_current_span('my-function'):
+        time.sleep(0.1)
     return {'statusCode': 200, 'body': 'ok'}
 
 if __name__ == '__main__':
