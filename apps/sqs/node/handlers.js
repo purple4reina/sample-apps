1608c1059e7f0169984a90b9f26e52be5f173cb1
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-07-26 14:27:11 -0700

    Send current trace id for node producer.

--- apps/sqs/node/handlers.js
+++ apps/sqs/node/handlers.js
@@ -1,3 +1,5 @@
+const tracer = require('dd-trace').init();
+
 const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
 const client = new SQSClient();
 
@@ -5,17 +7,22 @@ const queueUrls = process.env.SQS_QUEUE_URLS.split(',');
 const runtime = process.env.AWS_EXECUTION_ENV.replace('AWS_Lambda_', '');
 
 exports.producer = async function(event, context) {
-  console.log("queueUrls: ", queueUrls);
   msg = JSON.stringify({
     runtime: runtime,
-    trace_id: 'abcdefg',
+    trace_id: currentTraceId(),
   })
   for (url of queueUrls) {
-    console.log(`sending sqs message ${msg} to ${url}`)
+    console.log(`sending sqs message ${msg} to ${url}`);
     await client.send(new SendMessageCommand({
       MessageBody: msg,
       QueueUrl: url,
     }));
   }
   return {'statusCode': 200, 'body': 'ok'};
+}
+
+function currentTraceId() {
+  const ctx = tracer.scope().active()?.context();
+  console.log(`found trace context: traceId=${ctx.toTraceId()} spanId=${ctx.toSpanId()}`);
+  return ctx.toTraceId();
 }
