08a278681eb96514d30e5a019d3aeb4fb4f9409f
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-08-02 10:42:37 -0700

    Chalice app.

---
+++ python/chalice/app.py
@@ -0,0 +1,13 @@
+from chalice import Chalice, ConvertToMiddleware
+from datadog_lambda.wrapper import datadog_lambda_wrapper
+
+app = Chalice(app_name='rey-python-chalice')
+app.register_middleware(ConvertToMiddleware(datadog_lambda_wrapper))
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+@app.schedule('rate(1 minute)')
+def run_cron(event):
+    return {'hello': 'schedule'}
