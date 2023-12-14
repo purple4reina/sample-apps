c71962787886eca7328314148525fb9b060e1748
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-12-14 11:55:11 -0800

    Simplify handler.

--- python/lambda/handler.py
+++ python/lambda/handler.py
@@ -1,7 +1,2 @@
 def handler(event, context):
-    print('handler received event:', event)
-    print('handler received context:', context)
-    return {
-            'statusCode': 200,
-            'body': '{"Hello": "World"}',
-    }
+    return 'ok'
