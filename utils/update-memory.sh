2cbae1cb6194b85489dc00587b6c9e1688abac71
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-12-14 11:55:39 -0800

    Switch aws accounts.

--- utils/update-memory.sh
+++ utils/update-memory.sh
@@ -6,7 +6,7 @@ if [[ -z $REGION ]]; then
     REGION=sa-east-1
 fi
 
-aws-vault exec serverless-sandbox-account-admin -- aws lambda update-function-configuration \
+aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda update-function-configuration \
     --function "arn:aws:lambda:$REGION:425362996713:function:$funcname" \
     --region $REGION \
     --memory-size $(( RANDOM % 1024 + 128 )) | jq '.MemorySize'
