e88e8e11676f1df8dbd285db302dc3da48f22dd5
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-12-18 14:53:53 -0800

    Smaller window for mem size changes.

--- utils/update-memory.sh
+++ utils/update-memory.sh
@@ -9,4 +9,4 @@ fi
 aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda update-function-configuration \
     --function "arn:aws:lambda:$REGION:425362996713:function:$funcname" \
     --region $REGION \
-    --memory-size $(( RANDOM % 1024 + 128 )) | jq '.MemorySize'
+    --memory-size $(( RANDOM % 64 + 1024 )) | jq '.MemorySize'
