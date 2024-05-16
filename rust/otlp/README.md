568cf222358206731322457124be5e8797a2aec3
Rey Abolofia <rey.abolofia@datadoghq.com>
2024-05-16 10:24:45 -0700

    Use customer repro to add otel instrumnetation.

--- rust/otlp/README.md
+++ rust/otlp/README.md
@@ -1,26 +1,38 @@
-# Rust Lambda
+## What is this project?
 
-1. Install
-    ```bash
-    $ brew tap cargo-lambda/cargo-lambda
-    $ brew install cargo-lambda
-    ```
+This project is a demonstration of an issue where spans are missing in AWS Lambda function, which is implemented in Rust
+and packaged as a Docker image.
+When invoked, it should generate a span for the invocation, the handler function, and a pseudo-processing function (with
+five outer and inner spans each)
+, totaling 27 (1 + 1 + 5 * 5) spans.
 
-2. Create
-    ```bash
-    $ cargo lambda new YOUR_FUNCTION_NAME
-    ```
+## How to package the project as Docker image
 
-3. Build
-    ```bash
-    $ cargo lambda build --release
-    ```
+To package this project as a Docker image, execute the following command: (For Mac M1)
 
-4. Deploy
-    ```bash
-    $ aweserv cargo lambda deploy --region sa-east-1
-    ```
-    or
-    ```bash
-    $ aweserv sls deploy
-    ```
+```bash
+docker buildx build --build-context messense/rust-musl-cross:arm64-musl=docker-image://messense/rust-musl-cross:aarch64-musl --file ./Dockerfile --platform linux/arm64 --tag ${TAG_TO_PUSH_TO_ECR} .
+docker pushd ${TAG_TO_PUSH_TO_ECR}
+``` 
+
+Ensure you replace ${TAG_TO_PUSH_TO_ECR} with the tag you intend to push to your ECR registry.
+
+## How to run the app
+
+To run the application on AWS Lambda:
+
+1. Deploy the Docker image to AWS Lambda, ensuring that you select the arm64 architecture.
+
+2. Configure the following environment variables in the Lambda function:
+
+- `DD_API_KEY`
+- `DD_ENV`
+- `DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT`: Set this as `localhost:4317`
+- `DD_TRACE_OTEL_ENABLED`: Set this as `true`
+- `DD_SERVERLESS_FLUSH_STRATEGY`: Set this as `end`
+- `SAMPLING_RATIO`: sampling rate for the traces
+- `SERVICE_NAME`: name of the service
+- `URL`: URL of the receiver of otlp payload, sent from the application
+
+3. Invoke the Lambda function using the Test Event feature in the AWS Console. Select the apigateway-http-api-proxy
+   template to generate the test event for invoking the function.
--- rust/otlp/src/main.rs
+++ rust/otlp/src/main.rs
@@ -1,30 +1,91 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+use lambda_http::{Adapter, Body, Error, lambda_runtime, Request, Response};
+use lambda_http::http::StatusCode;
+use lambda_http::tower::{ServiceBuilder, ServiceExt};
+use lambda_http::tracing::instrument;
+use lambda_http::tracing::subscriber::{EnvFilter, Registry};
+use lambda_http::tracing::subscriber::layer::SubscriberExt;
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{Resource, trace};
+use opentelemetry_sdk::trace::Sampler;
+use opentelemetry_semantic_conventions::resource;
+use serde::Deserialize;
+use tracing::{info, info_span};
+use url::Url;
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+pub fn setup_tracer(conf: &Config) -> trace::TracerProvider {
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint(Url::parse(&conf.url.to_string()).unwrap()),
+        )
+        .with_trace_config(trace::config()
+            .with_sampler(Sampler::TraceIdRatioBased(conf.sampling_ratio))
+            .with_resource(Resource::new(vec![
+                KeyValue::new(resource::SERVICE_NAME, conf.service_name.to_string()),
+            ])))
+        .install_batch(opentelemetry_sdk::runtime::Tokio)
+        .expect("failed to install opentelemetry tracer");
+    let tracer_provider = tracer
+        .provider()
+        .expect("failed to get tracer provider, this should never happen because we just installed the tracer");
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
+    let telemetry_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = Registry::default()
+        .with(telemetry_layer)
+        .with(EnvFilter::new("info".to_string()));
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("failed to set global default tracing subscriber");
+    tracer_provider
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing::init_default_subscriber();
+    let conf = envy::from_env::<Config>()?;
+    let tracer_provider = setup_tracer(&conf);
 
-    run(service_fn(function_handler)).await
+    let service_fn = ServiceBuilder::new()
+        .layer_fn(Adapter::from)
+        .service_fn(|event: Request| f(event));
+    lambda_runtime::run(service_fn.then(|res| async {
+        for res in tracer_provider.force_flush() {
+            if let Err(e) = res {
+                info!("force flush error: {:?}", e);
+            }
+        }
+        res
+    }))
+        .await
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    service_name: String,
+    url: String,
+    sampling_ratio: f64,
+}
+
+#[instrument]
+pub async fn f(_: Request) -> Result<Response<Body>, Error> {
+    for i in 0..5 {
+        let i = i;
+        info_span!("outer", i = i)
+            .in_scope(|| async move {
+                for j in 0..5 {
+                    info_span!("inner", i = i, j = j)
+                        .in_scope(|| async move {
+                            println!("in span, {}-{}", i, j);
+                        })
+                        .await
+                }
+            })
+            .await
+    }
+
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::Empty)
+        .unwrap())
 }
