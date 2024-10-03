2b8e16cbd09be8834faeee755630bda7fcd0b69a
Rey Abolofia <rey.abolofia@datadoghq.com>
2024-10-02 17:56:01 -0700

    Fix exec script.

--- java/custom-metric/exec.sh
+++ java/custom-metric/exec.sh
@@ -4,10 +4,13 @@ while true
 do
     for _ in {1..50}
     do
-        curl https://oqrarzliowcus6dy6gztyazxc40bkkbf.lambda-url.sa-east-1.on.aws &
-        sleep 10
-        curl https://oqrarzliowcus6dy6gztyazxc40bkkbf.lambda-url.sa-east-1.on.aws &
+        (
+            curl https://kayyvs6owqwxmekjpi4kwuimyu0fctcj.lambda-url.sa-east-1.on.aws &
+            sleep 10
+            curl https://kayyvs6owqwxmekjpi4kwuimyu0fctcj.lambda-url.sa-east-1.on.aws &
+        ) &
     done &
 
     sleep 600
+    echo ; echo
 done
