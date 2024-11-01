a25c062370c523c41d3d18aa5ac4171e067e3b4e
Rey Abolofia <rey.abolofia@datadoghq.com>
2024-11-01 12:38:19 -0700

    Streaming java function.

--- java/stream-handler/src/main/java/example/StreamHandler.java
+++ java/stream-handler/src/main/java/example/StreamHandler.java
@@ -2,11 +2,13 @@ package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
-// Handler value: example.Handler
-public class MyStreamHandler implements RequestStreamHandler {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+// Handler value: example.StreamHandler
+public class StreamHandler implements RequestStreamHandler {
 
   @Override
   public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
