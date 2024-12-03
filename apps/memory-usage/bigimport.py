c3a1ebc11cc0ba9896efe56b53b0dc9c817be2b4
Rey Abolofia <rey.abolofia@datadoghq.com>
2024-12-03 11:15:40 -0800

    Simple high memory usage app.

---
+++ apps/memory-usage/bigimport.py
@@ -0,0 +1,2 @@
+bigimport = """
+This is a really big module that takes a long time to import.""" * 10_000_000
---
+++ apps/memory-usage/handler.py
@@ -0,0 +1,5 @@
+from bigimport import bigimport
+from slowimport import slowimport
+
+def handler(event, context):
+    return 'ok'
