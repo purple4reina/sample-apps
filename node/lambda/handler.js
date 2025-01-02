7aaf5e5de3757ced03dbeead0b0cf9dfd4e2da85
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-01-02 11:44:23 -0800

    Simplify handler.

--- node/lambda/handler.js
+++ node/lambda/handler.js
@@ -1,9 +1,3 @@
-var coldStart = true;
-
 exports.handler = async function(event, context) {
-  try {
-    return { cold_start: coldStart }
-  } finally {
-    coldStart = false;
-  }
+  return "ok";
 }
