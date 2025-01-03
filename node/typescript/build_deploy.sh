dfbed729f9a9d17edf68d1a12ae3d7f31bdfa731
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-01-03 11:28:50 -0800

    Create custom metric.

--- node/typescript/build_deploy.sh
+++ node/typescript/build_deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
 ./node_modules/.bin/esbuild \
-    handler.ts --bundle --minify --sourcemap --platform=node --target=es2020 \
+    handler.ts \
+        --bundle \
+        --minify \
+        --sourcemap \
+        --platform=node \
+        --target=es2020 \
+        --external:datadog-lambda-js \
         --outfile=handler.js
 
 aws-vault exec sso-serverless-sandbox-account-admin -- sls deploy
--- node/typescript/handler.ts
+++ node/typescript/handler.ts
@@ -1,5 +1,7 @@
 import { Handler } from 'aws-lambda';
+import { sendDistributionMetric } from 'datadog-lambda-js';
 
 export const handler: Handler = async (event, context) => {
+    sendDistributionMetric('rey.ts.kittens', 1);
     return JSON.stringify(event, null, 2);
 };
