c3ebc7adb98cd803b8aba4d5e06e8cdc644f53da
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-01-06 09:07:27 -0800

    Use ExcludeAssets.

--- dotnet/custom-span/build_deploy.sh
+++ dotnet/custom-span/build_deploy.sh
@@ -4,8 +4,6 @@ echo "📦 packaging release"
 echo
 rm -rf .package
 dotnet lambda package --configuration Release --output-package .package/handler.zip
-echo "🧹 removing tracer from package"
-zip -d .package/handler.zip Datadog.Trace.dll
 echo
 echo "🚀 deploying package"
 aws-vault exec sso-serverless-sandbox-account-admin -- serverless deploy
