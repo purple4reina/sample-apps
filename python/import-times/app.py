ab7074621821b2fc505cb4a9c54571595158f0ef
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-01-09 12:41:06 -0800

    Add standard ev vars.

--- python/import-times/app.py
+++ python/import-times/app.py
@@ -4,6 +4,8 @@ import time
 start = time.time_ns()
 os.environ['DD_ENV'] = 'rey'
 os.environ['DD_SERVICE'] = 'rey-local-app'
+os.environ['DD_INSTRUMENTATION_TELEMETRY_ENABLED'] = 'false'
+os.environ['DD_API_SECURITY_ENABLED'] = 'false'
 
 from datadog_lambda.cold_start import ColdStartTracer
 from ddtrace import patch_all, tracer
