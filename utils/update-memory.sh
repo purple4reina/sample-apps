36dfba43210a9ae78c6d06c6bf0682ddb60b269b
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-02-14 12:14:24 -0800

    Add option for account.

--- utils/update-memory.sh
+++ utils/update-memory.sh
@@ -5,9 +5,12 @@ funcname=$1
 if [[ -z $REGION ]]; then
     REGION=sa-east-1
 fi
+if [[ -z $ACCOUNT ]]; then
+    ACCOUNT=425362996713
+fi
 
 aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda update-function-configuration \
-    --function "arn:aws:lambda:$REGION:425362996713:function:$funcname" \
+    --function "arn:aws:lambda:$REGION:$ACCOUNT:function:$funcname" \
     --region $REGION \
     --memory-size $(( RANDOM % 64 + 1024 )) \
     --query MemorySize
