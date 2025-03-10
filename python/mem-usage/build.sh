8e8ec30b1d7b52fb56ccb38db422eff9c7fa40d5
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-03-10 11:32:45 -0700

    more goodies

--- python/mem-usage/build.sh
+++ python/mem-usage/build.sh
@@ -2,21 +2,25 @@
 
 # build layers
 docker_id=$(
-    docker run -d public.ecr.aws/sam/build-python3.12 \
-        pip install numpy matplotlib \
-            -t /tmp/deps \
-            --no-cache-dir \
-            --root-user-action ignore)
+    docker run -d \
+        -e PYTHONDONTWRITEBYTECODE=1 \
+        public.ecr.aws/sam/build-python3.12 \
+            pip install numpy matplotlib \
+                -t /tmp/deps \
+                --no-cache-dir \
+                --root-user-action ignore)
 
 while docker inspect $docker_id --format '{{.State.Running}}' | grep true &>/dev/null; do
     echo -n . && sleep 1
 done
 
-rm -rf python
+rm -rf python numpy-deps.zip
 mkdir -p python/lib/python3.12/site-packages/
 
 docker cp $docker_id:/tmp/deps/. python/lib/python3.12/site-packages/
 docker rm $docker_id
+
+find python -type d -name '__pycache__' -exec rm -rf {} +
 
 zip -r numpy-deps.zip python
 rm -rf python
