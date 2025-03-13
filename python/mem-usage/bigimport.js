89e49afaff24d9dc81b37f4272be4c0c19068ce2
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-03-13 13:16:06 -0700

    Add nodejs function.

---
+++ python/mem-usage/bigimport.js
@@ -0,0 +1,5 @@
+let bigVariable = 'big';
+for (let i = 0; i < 10000000; i++) {
+  bigVariable += 'big';
+}
+exports.bigVariable = bigVariable;
---
+++ python/mem-usage/handler.js
@@ -0,0 +1,5 @@
+const bigImport = require('./bigimport.js');
+
+exports.handler = async function(event, context) {
+  return "ok";
+}
