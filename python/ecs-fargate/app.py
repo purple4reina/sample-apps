a7bfbdb382eb4adeff393bbdb763d7a105b7c7e9
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-04-16 09:33:07 -0700

    Change name to include "Rey".

--- python/ecs-fargate/app.py
+++ python/ecs-fargate/app.py
@@ -7,7 +7,7 @@ from ecs_fargate.ecs_fargate_stack import EcsFargateStack
 
 
 app = cdk.App()
-EcsFargateStack(app, "EcsFargateStack",
+EcsFargateStack(app, "ReyEcsFargateStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
