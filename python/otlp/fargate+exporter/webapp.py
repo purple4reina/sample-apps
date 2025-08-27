33f57307c9c94cd935eafc8556ef5a1a24e7fdd7
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-08-27 14:24:10 -0700

    Failed attempts to get it working.

--- python/otlp/fargate+exporter/webapp.py
+++ python/otlp/fargate+exporter/webapp.py
@@ -1,7 +1,9 @@
-import flask
-
-app = flask.Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'ok'
+def app(environ, start_response):
+    data = b'ok'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
