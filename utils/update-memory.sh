366e6b5908ac04e36c12fa678362cacb28f17113
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-12-15 11:51:45 -0800

    Consistent memsize values.

--- utils/update-memory.sh
+++ utils/update-memory.sh
@@ -9,8 +9,20 @@ if [[ -z $ACCOUNT ]]; then
     ACCOUNT=425362996713
 fi
 
+memsize=$(
+    aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda get-function-configuration \
+        --function-name "$funcname" \
+        --region $REGION \
+        --query MemorySize
+)
+if [ $(( memsize % 2 )) = 0 ]; then
+    memsize=$(( memsize + 1 ))
+else
+    memsize=$(( memsize - 1 ))
+fi
+
 aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda update-function-configuration \
     --function "arn:aws:lambda:$REGION:$ACCOUNT:function:$funcname" \
     --region $REGION \
-    --memory-size $(( RANDOM % 64 + 1024 )) \
+    --memory-size $memsize \
     --query MemorySize
