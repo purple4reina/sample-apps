1efb5d7f2251bc6e07eb7fc0c11ef399af286d94
Rey Abolofia <rey.abolofia@datadoghq.com>
2026-06-03 15:47:09 -0700

    Remove bootstrap file.

--- go/lambda/build_deploy.sh
+++ go/lambda/build_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "📦 packaging release"
-rm -rf bin
+rm -rf bin bootstrap
 CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -o bootstrap handler.go
 echo -n "🚀 deploying package"
 aws-vault exec sso-serverless-sandbox-account-admin -- sls deploy
